6d11aec002eb12d953e648db4454e15026456638
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2024-11-25 13:06:53 +0100

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Check if virtual environment folder exists
+sudo apt install python3.9-venv
+
 if [ ! -d "venv" ]; then
     echo "Virtual environment not found. Creating one..."
-    python3 -m venv venv
+    python3.9 -m venv venv
 fi
 
 # Activate the virtual environment
@@ -11,10 +13,10 @@ source venv/bin/activate
 
 # Install required Python packages
 echo "Installing dependencies..."
-    pip install tabulate
+    sudo apt install python3-tabulate -y
     sudo apt install python3-tk -y
     clear
 
 # Run the script
 echo "Running the script..."
-python3 DVR_view.py
+python3.9 DVR_view.py
