98c65f305b22474da6d34128b63fbe24b43e3971
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2024-11-26 16:52:17 +0100

    upgrading pip in start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,9 @@
 @echo off
 REM Install required Python packages
 echo Installing dependencies...
+python -m pip install --upgrade pip
 pip install tabulate tk
 
 REM Run the script
 echo Running the script...
 python .\DVR_view.py
-
-pause
